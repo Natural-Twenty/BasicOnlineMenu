61cf5fa7800ca13d2cfd74ec9a3cddbfcc63c885
HarrisunW <z5177881@student.unsw.edu.au>
2019-04-20 20:45:58 +1000

    Update new changes

--- Class files/Assignment/src/Sides.py
+++ Class files/Assignment/src/Sides.py
@@ -8,10 +8,10 @@ Created on Fri Apr  5 15:29:33 2019
 
 class Sides:
     
-    def __init__(self, sides = ['3 pack nuggets', '6 pack nuggets', '0.2 kg small fries', '0.4 kg medium  fries', '0.6 kg large fries'],
-                 drinks = ['375mL can of coke', '600mL bottle of water'],
-                 sidePrice = [3, 5, 2, 3, 4],
-                 drinkPrice = [2, 3]):
+    def __init__(self, sides = ['3 pack nuggets', '6 pack nuggets', '0.2 kg small fries', '0.4 kg medium  fries', '0.6 kg large fries', 'Small chocolate sundae', 'Medium chocolate sundae', 'Large chocolate sundae', 'Small strawberry sundae', 'Medium strawberry sundae', 'Large strawberry sundae'],
+                 drinks = ['Can of coke', 'Bottle of coke' 'Bottle of water', 'Small orange juice', 'Medium orange juice', 'Large orange juice'],
+                 sidePrice = [3, 5, 2, 3, 4, 4, 5, 6, 4, 5, 6],
+                 drinkPrice = [2, 4, 3, 4, 5, 6]):
         self._sides = sides
         self._drinks = drinks
         self._sidePrice = sidePrice
